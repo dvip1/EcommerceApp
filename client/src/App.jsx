d8a23248eb14047705506b2171f5f0fd618101cb
dvip1 <dvip.d.patel@slrtce.in>
2023-02-25 20:17:31 +0530

    making it work with django

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,45 @@
-import React from "react"
+//import React from "react"
 import Form from "./form"
-import SignForm from "./signupform"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+//import SignForm from "./signupform"
+//import { BrowserRouter,Routes,Route } from "react-router-dom"
+//
 
+// function App() {
+  
+
+//   return (
+//   <div className="App flex justify-center items-center min-h-screen bg-slate-400">
+//   <BrowserRouter>
+//   <Routes>
+//     <Route path="/" element={<Form/>}></Route>
+//     <Route path="signupform" element={<SignForm/>}></Route>
+
+//   </Routes>
+
+//   </BrowserRouter>
+  
+//     </div>
+//   )
+// }
+
+// export default App
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/hello/')
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
-  <div className="App flex justify-center items-center min-h-screen bg-slate-400">
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Form/>}></Route>
-    <Route path="signupform" element={<SignForm/>}></Route>
-
-  </Routes>
-
-  </BrowserRouter>
-  
+    <div>
+      <h1>{message}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
